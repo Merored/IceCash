ab4f1ea9463d61ee6594514bc9988b152897a29a
kassir <kassir@kassir.(none)>
2016-03-22 19:51:57 +0300

    Добавил очистку висящего ответа ККМ

--- bin/kkmdrv-atol.py
+++ bin/kkmdrv-atol.py
@@ -5,10 +5,18 @@ import time
 import sys
 import string
 
-const_error=1
-const_cmd={'sale':11,'return':13,'X':60,'Z':61,'close_check':55,'cancel_check':56,}
-MAX_TRIES = 10
-MIN_TIMEOUT = 0.05
+
+MAX_TRIES = 5
+T1 = 0.5
+T2 = 2
+T3 = 0.5
+T4 = 0.5
+T5 = 10
+T6 = 0.5 
+T7 = 0.5
+T8 = 1
+
+
 OS_CP = 'cp866'
 PORT = '/dev/ttyS0' #'COM1'
 ENQ = chr(0x05)
@@ -27,6 +35,17 @@ def bufStr(*b):
     for x in b: result.append(chr(x))
     return string.join(result,'')
 
+def hexStr(s):
+    """Преобразуем в 16-е значения"""
+    result = []
+    for c in s: result.append(hex(ord(c)))
+    return string.join(result,' ')
+
+def float2100int(f,digits=2):
+        mask = "%."+str(digits)+'f'
+        s    = mask % f
+        return int(s.replace('.',''))
+
 def LRC(buff):
     """Подсчет CRC"""
     result = 0
@@ -35,14 +54,27 @@ def LRC(buff):
     print( "LRC",result)
     return result
 
-def hexStr(s):
-    """Преобразуем в 16-е значения"""
-    result = []
-    for c in s: result.append(hex(ord(c)))
-    return string.join(result,' ')
-
-DEFAULT_ADM_PASSWORD = bufStr(0x1e,0x0,0x0,0x0) #Пароль админа по умолчанию = 30
+DEFAULT_ADM_PASSWORD = bufStr(0x00,0x1e) #Пароль админа по умолчанию = 30
 DEFAULT_PASSWORD     = bufStr(0x00,0x00)  #Пароль кассира по умолчанию = 0
+
+
+class kkmException(Exception):
+        def __init__(self, value):
+                self.value = value
+                self.s = { 0x88: "Смена превысила 24 часа (Закройте смену с гашением) (ошибка 0x88)",\
+                           0x8C: "Неверный пароль (ошибка 0x8C)",\
+                           0x7A: "Данная модель ККТ не может выполнить команду (ошибка 0x7A)",\
+                           0x9B: "Чек открыт – операция невозможна (ошибка 0x9B)",\
+                           0x9A: "Чек закрыт – операция невозможна (ошибка 0x9A)",\
+                           0x9C: "Смена открыта, операция невозможна (ошибка 0x9C)",\
+                           0x8F: "Обнуленная касса (повторное гашение невозможно) (ошибка 0x8F)",
+                        }[value]
+        def __str__(self):
+            return self.s
+
+        def __unicode__(self):
+            return unicode(str(self.s),'utf8')
+
 
 class KKM:
         def __init__(self,conn, password):
@@ -69,13 +101,39 @@ class KKM:
                     raise RuntimeError("Unknown answer")
 
 
+        def __clearAnswer(self):
+                """Сбросить ответ если он болтается в ККМ"""
+                def oneRound():
+                        self.conn.flush()
+                        self.conn.write(ENQ)
+                        a = self.conn.read(1)
+                        time.sleep(T7)
+                        if a==ACK:
+                                return 1
+                        elif a==STX:
+                                a = self.conn.read(1)
+                                time.sleep(T1)
+                                if a==STX:
+                                       time.sleep(T5)
+                                       self.conn.write(EOT)
+                                time.sleep(T1)
+                                self.conn.write(EOT)
+                                time.sleep(T1)
+                                return 2
+                        else:
+                                raise RuntimeError("Something wrong")
+                n=0
+                while n<MAX_TRIES and oneRound()!=1:
+                        n+=1
+                if n>=MAX_TRIES:
+                        return 1
+                return 0
+
 
         def __sendCommand(self,cmd,params):
                 """Стандартная обработка команды"""
                 self.conn.flush()
                 data   = DEFAULT_PASSWORD + chr(cmd) 
-
-            
                 try:
                     data = data + chr(params)
                 except:
@@ -95,7 +153,8 @@ class KKM:
 
         def Beep(self):
                 """Гудок"""
-                self.__sendCommand(0x47,''  )
+                self.__clearAnswer()
+                self.__sendCommand(0x47,'')
 
 
 try:
@@ -117,7 +176,7 @@ try:
     print("Beep") 
 except Exception as e: 
     print(e)
-    err=const_error
+    err= 1 
     traceback.print_exc(file=sys.stdout)
     #self.ser.close()//for renull
     print("not connect frk")
